7d84997fe0eafdac0c774dff7b7cb60c6ee068cd
kishorreyansh <kishor.andekar2389@gmail.com>
2025-02-16 00:22:37 -0600

    react-social-media-post-project

---
+++ react-social-media/src/store/post-list-store.jsx
@@ -0,0 +1,79 @@
+import { createContext } from "react";
+import { useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+  return newPostList;
+};
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    //console.log(`${userId} ${postTitle} ${postBody} ${reactions} ${tags}`);
+
+    const addPost = {
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    };
+    dispatchPostList(addPost);
+  };
+
+  const deletePost = (postId) => {
+    const deletePost = {
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    };
+    dispatchPostList(deletePost);
+  };
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Learning React",
+    body: "Learning React Concepts such as Context API and use Reducer and many more.",
+    reactions: 2,
+    userId: "user-1",
+    tags: ["react", "hooks"],
+  },
+  {
+    id: "2",
+    title: "Learning Spring Boot",
+    body: "Learning Spring Boot Concepts such as Context API and use Reducer and many more.",
+    reactions: 12,
+    userId: "user-2",
+    tags: ["spring", "boot"],
+  },
+];
+
+export default PostListProvider;
